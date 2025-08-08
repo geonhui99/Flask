bf89157294a5489baad9a211c491cb6f5d44d5c1
geonhui99 <leerjs77@naver.com>
2025-08-08 16:02:22 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    users = [
-        {"username": "traveler", "name": "Alex"},
-        {"username": "photographer", "name": "Sam"},
-        {"username": "gourmet", "name": "Chris"}
+    foods = [
+        {"name": "한우", "type": "고기"},
+        {"name": "아이스크림", "type": "디저트"},
+        {"name": "음료수", "type": "디저트"}
     ]
-    return render_template('index.html', users=users)
+    return render_template('index.html', foods=foods)
 
 if __name__ == '__main__':
     app.run(debug=True)
